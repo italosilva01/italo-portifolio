a9c4c501f2dbd2956dc2d6217adff75fc62a2eb6
Francisco Italo <italo.silvan87@gmail.com>
2024-06-20 22:49:14 -0300

    add resume

--- src/app/components/LetsTalk/index.tsx
+++ src/app/components/LetsTalk/index.tsx
@@ -5,14 +5,18 @@ export const LetsTalk = () => {
   return (
     <Button
       variant="outlined"
-      className="border-purple-dark/[0.5] text-purple-dark/[0.5] gap-2 md:rounded-xl"
+      sx={{
+        padding: 1,
+      }}
+      className="border-purple-dark/[0.5] text-purple-dark/[0.5] md:rounded-xl"
     >
       <a
         href={`https://api.whatsapp.com/send?phone=05584987589718`}
         target="_blank"
+        className="flex justify-center text-start gap-2 items-center"
       >
-        <Image src="/whatsappLogo.svg" alt={""} width={24} height={24} /> Vamos
-        conversar
+        <Image src="/whatsappLogo.svg" alt={""} width={24} height={24} />
+        <span>Vamos conversar</span>
       </a>
     </Button>
   );
